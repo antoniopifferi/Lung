081c8b2cc33d29993a2282e61a18df02406b3a48
Antonio Pifferi <antonio.pifferi@polimi.it>
2020-04-20 12:04:50 +0200

    Start in Python

---
+++ Lung.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 11:56:40 2020
+
+@author: anton
+"""
+from numpy import *
+from scipy import *
+from matplotlib.pyplot import *
+from pandas import *
+close('all')
+
